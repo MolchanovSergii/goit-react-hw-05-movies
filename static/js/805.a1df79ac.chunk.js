3cd82f8a13eae3b199cd3c6f09ec73198911ed75
MolchanovSergii <118202123+MolchanovSergii@users.noreply.github.com>
2023-06-23 13:17:15 +0000

    Deploying to gh-pages from @ MolchanovSergii/goit-react-hw-05-movies@d90316ad0e8c978ffcbc4177a4c84f899bf887b9 🚀

--- static/js/805.a1df79ac.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{9805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("h2",{children:"Sorry.Page not found"})}}}]);
-//# sourceMappingURL=805.a1df79ac.chunk.js.map
